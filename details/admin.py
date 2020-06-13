01fec6085087ee68b9c0e8c9a53ca2841767af4e
Adit-ahnaf <45269462+Adit-ahnaf@users.noreply.github.com>
2020-06-13 14:27:45 +0600

    add a search field in the admin pannel

--- details/admin.py
+++ details/admin.py
@@ -1,4 +1,11 @@
 from django.contrib import admin
 from .models import Book
 # Register your models here.
-admin.site.register(Book)
+class BookModelAdmin(admin.ModelAdmin):
+	list_display = ["title"]
+	search_fields = ["title"]
+class Meta:
+	model = Book
+
+
+admin.site.register(Book, BookModelAdmin)
